b556b2443468b071cef66459f1fe44c87f5153fc
YEAHx4 <developerstarlight@gmail.com>
2023-06-23 18:40:04 +0900

    Init kotlin spring boot project

---
+++ src/main/kotlin/io/yeahx4/rotcar/RotcarApplication.kt
@@ -0,0 +1,11 @@
+package io.yeahx4.rotcar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RotcarApplication
+
+fun main(args: Array<String>) {
+    runApplication<RotcarApplication>(*args)
+}
---
+++ src/test/kotlin/io/yeahx4/rotcar/RotcarApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.yeahx4.rotcar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RotcarApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
